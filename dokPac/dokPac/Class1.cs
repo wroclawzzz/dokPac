d6c9ca81ab821b4f9a8153ea6425245971262451
kasia111 <katarzyna.gostynska@wp.pl>
2014-05-07 11:09:09 +0200

    message

---
+++ dokPac/dokPac/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dokPac
+{
+    public class Class1
+    {
+        void a() { }
+        int b = 5;
+
+    }
+}
